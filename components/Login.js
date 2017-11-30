f8a0c0ff54db4f2916747e834f99423806c3871f
Ketevann <ketevan.tsin@gmail.com>
2017-11-30 18:08:14 -0500

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import firebase from 'firebase';
 import { Header, Button, Spinner } from './common';
-import {LoginForm} from './common/LoginForm'
+import LoginForm  from './common/LoginForm'
+import { loginDispatch, loginDispatchFalse } from '../redux/actions/authActions'
+import { connect } from 'react-redux'
 
-class App extends Component {
-  state = { loggedIn: null }
+class Login extends Component {
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBxQ6Lpnp0FT8Fvg8g8Ic7_w4PDJwnqmMo',
@@ -16,34 +18,50 @@ class App extends Component {
       messagingSenderId: '314366021514'
     });
     firebase.auth().onAuthStateChanged((user) => {
+      console.log((this.props, ' in authfirebase', user))
       if (user) {
-        this.setState({ loggedIn: true })
+        this.props.loginDispatch()
       }
-      else this.setState({ loggedIn: false })
+      else this.props.loginDispatchFalse()
     })
   }
 
-renderContent(){
-    switch(this.state.loggedIn){
-      case true:
-       return (
-         <Button onPress={() => firebase.auth().signOut()}
-       >Log Out</Button>)
-      case false: return  <LoginForm />
-      default:
-      return <Spinner size="small" />
+  renderContent() {
+    console.log(this.props.auth.loggedIn, 'logged in')
+    const { loggedIn } = this.props.auth
+    console.log(loggedIn, '***');
+    if (loggedIn) {
+
+        console.log(loggedIn, 'true');
+        return (
+          <Button onPress={() =>
+            firebase.auth().signOut()}>
+            Log Out</Button>)
+      }
+      else if (loggedIn === false){
+        return <LoginForm />
+      }
+
+      else {
+        return <Spinner size='large' />
+      }
+
     }
 
 
-}
-  render() {
-    return (
-      <View>
-        <Header headerText="Authentication" />
-       {this.renderContent()}
-      </View>
-    );
-  }
-}
 
-export default App;
+    render() {
+      { console.log('props', this.props, this.state) }
+      return (
+        <View>
+          <Header headerText="Authentication" />
+          {this.renderContent()}
+        </View>
+      );
+    }
+  }
+
+  export default connect(
+    ({ auth }) => ({ auth: auth }),
+    { loginDispatch, loginDispatchFalse },
+  )(Login)
