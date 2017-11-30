53793197c3279fb6e926cc969686183cd8e1963b
Ketevann <ketevan.tsin@gmail.com>
2017-11-30 17:42:16 -0500

    Create auth.js

---
+++ redux/reducers/auth.js
@@ -0,0 +1,36 @@
+const INITIAL_STATE = { email: '', password: '', error: '', loading: false, loggedIn: null }
+import {
+  EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCESS, LOGIN_USER_FAIL,
+  LOGIN_USER,
+  LOGGEDIN,
+  NOTLOGGEDIN,
+  FORGOT
+} from '../actions/action-types'
+
+
+
+export default (auth = INITIAL_STATE, action) => {
+  console.log(action, 'ACTIONNN');
+  switch (action.type) {
+    case EMAIL_CHANGED:
+      return { ...auth, email: action.payload };
+    case PASSWORD_CHANGED:
+      console.log('passowrd', action)
+
+      return { ...auth, password: action.payload };
+    case LOGIN_USER_SUCESS:
+      return { ...auth, ...INITIAL_STATE, user: action.payload };
+    case LOGIN_USER_FAIL:
+      return { ...auth, error: "Authentication Failed", loading: false };
+    case LOGIN_USER:
+      return { ...auth, loading: true, error: '' };
+    case LOGGEDIN:
+      return {loggedIn: true };
+    case NOTLOGGEDIN:
+      return { ...auth, loggedIn: false };
+      case FORGOT:
+      return { ...auth, email: '', error: '' };
+    default:
+      return auth
+  }
+}
