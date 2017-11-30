591fd67a0d674ba09b848780d420ba7d2889a1b2
Ketevann <ketevan.tsin@gmail.com>
2017-11-30 17:41:45 -0500

    Create index.js

---
+++ redux/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import auth from './auth'
+import reducers from './reducers';
+
+
+export default combineReducers({
+  auth: auth
+  //reducers: reducers
+});
