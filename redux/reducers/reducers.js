855cbb7c3b90215ae1ede4bd7a1844ed055986bf
Ketevann <ketevan.tsin@gmail.com>
2017-11-30 19:34:13 -0500

    testing

--- redux/reducers/reducers.js
+++ redux/reducers/reducers.js
@@ -1,23 +1,23 @@
 
 import * as types from '../actions/action-types';
 
- const reducers = (state = initialState, action) => {
+ const reducers = (state = {}, action) => {
 
     switch (action.type) {
 
         case types.GET_BOOKLIST_DATA_RECIEVED:
             console.log("GET_BOOKLIST_DATA_RECIEVED");
             return Object.assign({}, state, {
-                bookListData: action.data,  
+                bookListData: action.data,
             })
         case types.DEFAULT_BOOKLIST_DATA_RECIEVED:
             console.log("DEFAULT_BOOKLIST_DATA_RECIEVED");
             return Object.assign({}, state, {
-                defaultBookList: action.data,  
+                defaultBookList: action.data,
             })
 
-        
-            
+
+
         default:
             return state
     }
