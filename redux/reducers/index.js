e1ad4ed623b6fe10f0b003ea2170a3f2c215cae7
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-12-04 19:59:50 -0500

    added preferences reducer to combine reducers

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,9 +1,10 @@
 import {combineReducers} from 'redux';
 import auth from './auth'
 import defaultBookListReducer from './defaultBookListReducer';
-
+import preferences from './preferences';
 
 export default combineReducers({
   auth: auth,
-  defaultBookList: defaultBookListReducer
+  defaultBookList: defaultBookListReducer,
+  preferences: preferences
 });
