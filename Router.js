d287503f651f877f032e1d5622cd8607da98fd82
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-12-04 21:10:27 -0500

    added preferences route

--- Router.js
+++ Router.js
@@ -3,7 +3,7 @@ import {Scene, Router, Actions, Stack} from 'react-native-router-flux';
 import Login from './components/Login'
 import Home from './components/Home'
 import ForgotPassword from './components/ForgotPassword'
-
+import PreferencesForm from './components/common/PreferencesForm'
 
 
 
@@ -13,6 +13,7 @@ const RouterComponent = () => (
       <Scene key="login" component={Login} title="Login" />
       <Scene key="home" component={Home} initial />
       <Scene key="forgotpassowrd" component={ForgotPassword} />
+      <Scene key="preferencesForm" component={PreferencesForm} />
     </Stack>
   </Router>
 );
