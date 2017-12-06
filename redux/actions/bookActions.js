2f8cc152fced0ba2602595cf1bb74eda4a5527fd
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-12-06 13:52:36 -0500

    added dispatcher

--- redux/actions/bookActions.js
+++ redux/actions/bookActions.js
@@ -5,40 +5,33 @@ import {
 import firebase from 'firebase';
 
 
-export const getBookList = (title) => {
-  console.log("getBookListDataRedux",title);
-  return {
-    type: GET_BOOKLIST_DATA,
-    payload:title
-  };
-};
+export const getBookList = (title, dispatch) =>
+    dispatch => dispatch({
+                    type: GET_BOOKLIST_DATA,
+                    payload:title
+                });
 
 
-
-export const loadDefaultBookList = (bookList) => {
-  console.log("loadDefaultBookListData",bookList);
-  return {
-    type: LOAD_DEFAULT_BOOKLIST_DATA,
-    payload:bookList
-  };
-};
-
-
+export const loadDefaultBookList = (bookList, dispatch) => 
+    dispatch => dispatch({
+                    type: LOAD_DEFAULT_BOOKLIST_DATA,
+                    payload:bookList
+                });
 
 /* Saving books*/ 
 export const startNewSaveBookList = ( title, userID , dispatch) => 
-    dispatch=>{
+    dispatch => {
         // alert('start a new list');
         firebase.database().ref(`users/${userID}/`).set({ books:[title]})
-}
+    }
       
 export const appendSaveBookList = ( title, userID, dispatch ) => 
     dispatch => firebase.database().ref(`users/${userID}/books`).once('value', (snapshot)=>{
-                // alert(snapshot.val());
-                const savedBook=Object.values(snapshot.val());//get books already in db
-                firebase.database().ref(`users/${userID}/`).set({books:[... savedBook, title ]});//add new book and rest books branch
-                // console.log (savedBook, "------------->>>books")
-            });
+                    // alert(snapshot.val());
+                    const savedBook=Object.values(snapshot.val());//get books already in db
+                    firebase.database().ref(`users/${userID}/`).set({books:[... savedBook, title ]});//add new book and rest books branch
+                    // console.log (savedBook, "------------->>>books")
+                });
 
 
     
