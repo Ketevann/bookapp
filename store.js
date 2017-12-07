0d6725e918530a1e4e8ecc62b61783f7413dc5cc
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-12-06 20:30:03 -0500

    removing redux middleware

--- store.js
+++ store.js
@@ -2,12 +2,11 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './redux/reducers'
 import {createLogger} from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
-import { apiMiddleware } from './redux/middleware';
 
 
 
 
 export default store = createStore(
-     rootReducer, {}, applyMiddleware(apiMiddleware, ReduxThunk, createLogger({collapsed: true})));
+     rootReducer, {}, applyMiddleware( ReduxThunk, createLogger({collapsed: true})));
 
 
