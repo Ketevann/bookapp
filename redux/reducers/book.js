c60b3568dce622d056e84e66544312cefd5497b4
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-12-07 02:52:13 -0500

    removed obsolete actions

--- redux/reducers/book.js
+++ redux/reducers/book.js
@@ -1,25 +1,19 @@
 import {
-  BOOK_SAVED_SUCESSFULLY,
+  BOOK_SAVED,
   BOOK_NOT_SAVED,
-  GET_BOOKLIST_DATA_RECIEVED,
-  DEFAULT_BOOKLIST_DATA_RECIEVED
+  BOOK_SUGGESTIONS_DATA_RECIEVED
 } from '../actions/action-types'
 
-const INITIAL_STATE = { saved:null, bookListData:null, defaultBookList:null, error:''}
+const INITIAL_STATE = { saved:null, bookSuggestions:null, error:''}
 
 export default (book = INITIAL_STATE, action) => {
-  console.log(action, 'ACTIONNN');
   switch (action.type) {
-    case BOOK_SAVED_SUCESSFULLY:
+    case BOOK_SAVED:
       return {...book, saved: true }
     case BOOK_NOT_SAVED:
-      return {...book, saved: false, error: 'please login'}
-    case GET_BOOKLIST_DATA_RECIEVED:
-      // console.log("GET_BOOKLIST_DATA_RECIEVED");
-      return {...book, bookListData: action.payload }
-    case DEFAULT_BOOKLIST_DATA_RECIEVED:
-      // console.log("DEFAULT_BOOKLIST_DATA_RECIEVED");
-      return {...book, defaultBookList: action.payload }
+      return {...book, saved: false, error: 'error saving book'}
+    case BOOK_SUGGESTIONS_DATA_RECIEVED:
+      return {...book, bookSuggestions: action.payload }
     default:
       return book
   }
