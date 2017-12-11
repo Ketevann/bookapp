70b91c9e878364594993e3d5ad12beacce1ce513
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-12-11 02:47:17 -0500

    removed switch logic from getPreferences

--- redux/actions/preferencesActions.js
+++ redux/actions/preferencesActions.js
@@ -2,8 +2,8 @@ import {
     UPDATE_TITLE,
     UPDATE_AUTHOR,
     UPDATE_GENRE,
-    UPDATE_PREFERENCES_FIREBASE,
-    FETCHED_SAVED_PREFERENCES
+    UPDATE_PREFERENCES,
+    FETCHED_BOOKS
 } from './action-types'
 import firebase from 'firebase';
 import axios from 'axios';
@@ -12,58 +12,68 @@ import { GOOGLE_API_KEY } from '../../keys'
 
 
 
-export const updatedTitle = (title, dispatch) =>
+export const updateTitle = (title, dispatch) =>
   //console.log('UPDATE TITLE',title);
   dispatch => dispatch({
     type: UPDATE_TITLE,
     payload: title
   })
 
-export const updatedAuthor = (author, dispatch) =>
+export const updateAuthor = (author, dispatch) =>
   //console.log('UPDATE AUTHOR',author);
   dispatch => dispatch({
     type: UPDATE_AUTHOR,
     payload: author
   })
 
-export const updatedGenre = (genre, dispatch) =>
+export const updateGenre = (genre, dispatch) =>
   //console.log('UPDATE GENRE',genre);
   dispatch => dispatch({
     type: UPDATE_GENRE,
     payload: genre
   })
 
-export const updatedPreferencesFireBase = (preferences, userID, dispatch) =>
-  dispatch => {
-      console.log('UPDATE ALL PREFERENCES FireBase', preferences);
-      const {author, genre, title} = preferences
-      firebase.database().ref(`users/${userID}`).child(`preferences`).set({author, title, genre});
-      dispatch({ type: UPDATE_PREFERENCES_FIREBASE })
-  }
+export const updatePreferences = (preferences, userID, dispatch) =>
+    dispatch => {
+        console.log('UPDATE ALL PREFERENCES FireBase', preferences);
+        const {author, genre, title} = preferences
+        firebase.database().ref(`users/${userID}`).child(`preferences`).set({author, title, genre});
+        dispatch({ type: UPDATE_PREFERENCES })
+    }
 
-            
-export const findSimilar = (keyword, type, dispatch) => axios.get(`https://tastedive.com/api/similar?q=${keyword}&k=${TASTE_DIVE_API_KEY}&limit=2&type=${type}s`)
-            .then(res => {
-                const bookPromises = res.data.Similar.Results.map((book) => axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book.Name}&key=${GOOGLE_API_KEY}`));
-                      axios.all(bookPromises).then(axios.spread((...args) => args.map((book) =>book.data.items[0].volumeInfo)))
-                       .then((res)=>{
-                          return  dispatch({ type: FETCHED_SAVED_PREFERENCES, payload: res }) //store.dispatch?
-                        })
-            })
 
-export const getPreferences = (userID, dispatch) =>
-  dispatch => firebase.database().ref(`users/${userID}/preferences`).once('value', (snapshot) => {
-        const preferences= snapshot.val();
-        for (key in preferences) {
-                switch(key) {
-                case 'title':
-                    preferences[key] ? findSimilar(preferences[key], 'book', dispatch) : null
-                    break;
-                case 'author':
-                    preferences[key] ? findSimilar(preferences[key], 'author', dispatch) : null
-                    break;
-                default:
-                    console.log('blank');
-              }  
+export const getBooksData = (books, dispatch) => {
+    const bookPromises = books.map((book) => 
+        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book.Name}&key=${GOOGLE_API_KEY}`));
+        axios.all(bookPromises)
+            .then(axios.spread((...args) => args.map((book) => book.data.items[0].volumeInfo))).then((res)=> dispatch({ type: FETCHED_BOOKS, payload: res })) //save to state
+                .catch((error) => {
+                    console.error(error);
+                });
+    }
+ 
+
+export const findSimilar = (keyword, type, dispatch) => {
+    var mediaType=null;
+    type==='title'? mediaType='book': mediaType='author';
+    axios.get(`https://tastedive.com/api/similar?q=${keyword}&k=${TASTE_DIVE_API_KEY}&limit=2&type=${mediaType}s`)
+        .then(res => getBooksData(res.data.Similar.Results, dispatch))
+            .catch((error) => {
+                console.error(error);
+            });
+}
+
+
+export const getPreferences = ( userID,dispatch ) =>
+    dispatch => firebase.database().ref(`users/${userID}/preferences`).once('value', (snapshot) => {
+        if (snapshot.val()){ //if preferences are saved then iterate object and get perefered books
+            const preferences= snapshot.val();
+            for (key in preferences) {
+                preferences[key] ? findSimilar(preferences[key], key, dispatch) : null;
             }
+        } else getBooksData(defaultBooks.list, dispatch)//load default books if there's no preferences saved
     })
+
+
+
+ 
