1627acbcac36519be0e994e2d52e88aa739f2630
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-01-03 04:39:00 -0500

    cleaning up code

--- redux/actions/preferencesActions.js
+++ redux/actions/preferencesActions.js
@@ -66,200 +66,152 @@ export const updatePreferences = (newPrefs, userID, dispatch) =>
 
                 console.log('display Suggestions ***')
 
-            })
+            })//*the glitch is here, since we send newPrefs to "find". if we only update one pref. the other isleft blank. and we over write suggestions, so we lose what was already there
             .catch(error => {
                 console.log('no prefs, starting new branch');
                 preferenceRef.set(newPrefs)
             })
             .then(() => {
-                //here we keep getting an array of promises, so promise.all fixes that  
-                 const promises=find(newPrefs, userID, dispatch);
-                        return Promise.all(promises)
-            }).then((res)=>{
-                //console.log( res, "------------------------->Origin")
-                //res here is an array of arrays. each child array has books data
-                    res.forEach((book)=>{
-                        if (book){//if the array child exists (cuz we get a "undefined" if there was no preference for that "keyword")
-                                saveSuggestions(book, userID, dispatch)
-                            }
-                    })
+                //we return a single array of titles to be sent to googleAPI
+                return find(newPrefs);//calling TasteDive
+            })
+            .then ((titles)=>{
+                //console.log(titles,"90909090909900==========9090909090============0990909090>")
+                return  getBooksFromApi(titles)//calling googleAPI
+            })
+            .then((booksData)=>{
+                  saveSuggestions(booksData, userID, dispatch)//saving
             })
     }
 
 
-export const displaySuggestions = (userID, dispatch) => {
-    console.log('display Suggestions *** nnnnn')
+// export const displaySuggestions = (userID, dispatch) => {
+//     console.log('display Suggestions *** nnnnn')
 
-        console.log('kkk')
-        // firebase.database().ref(`users/${userID}/suggestions`).once('value', (snapshot) => {
-        //     console.log('display Suggestions ***', snapshot.val())
-        //     const savedSuggestions = snapshot.val();
-        //     dispatch(hasSuggestions(savedSuggestions))
-        // });
+//         console.log('kkk')
+//         // firebase.database().ref(`users/${userID}/suggestions`).once('value', (snapshot) => {
+//         //     console.log('display Suggestions ***', snapshot.val())
+//         //     const savedSuggestions = snapshot.val();
+//         //     dispatch(hasSuggestions(savedSuggestions))
+//         // });
 
-}
+// }
 export const saveSuggestions = (books, userID, dispatch) => {
-    const suggestionsPref = firebase.database().ref(`users/${userID}/`).child('suggestions');
-    suggestionsPref.once('value')
-        .then(snapshot => {
-            if (!snapshot.val())
-                throw ("Error")
-            const savedSuggestions = Object.values(snapshot.val());
-            suggestionsPref.set([...savedSuggestions, ...books]);
-            dispatch({ type: UPDATE_SUGGESTIONS, payload: savedSuggestions })
-        })
-        .catch(error => {
-            console.log('34');
-            suggestionsPref.set([...books]);
+    // const suggestionsPref = firebase.database().ref(`users/${userID}/`).child('suggestions');
+    // suggestionsPref.once('value')
+    //     .then(snapshot => {//i dont think we append in this version of code, i think we can just, get rid of spread operator and throw/catch, that part was for appending.
+    //         if (!snapshot.val())
+    //             throw ("Error")
+                
+    //         const savedSuggestions = Object.values(snapshot.val());
+    //         suggestionsPref.set([...savedSuggestions, ...books]);
+    //         dispatch({ type: UPDATE_SUGGESTIONS, payload: savedSuggestions })
+    //     })
+    //     .catch(error => {
+    //         console.log('34');
+    //         suggestionsPref.set([...books]);
+    //         dispatch({ type: UPDATE_SUGGESTIONS, payload: books });
+    //     })
+    //     .then(()=>Actions.home());//instead of calling "display" we just redirect to home. 
+    
+    //i dont think we append in this version of code, i think we can just, get rid of spread operator and throw/catch, that part was for appending.
+     const suggestionsPref = firebase.database().ref(`users/${userID}/`).child('suggestions');
+        suggestionsPref.set([...books])
+        .then(()=>{
             dispatch({ type: UPDATE_SUGGESTIONS, payload: books });
         })
-       //.then(()=> dispatch({ type: UPDATE_SUGGESTIONS, payload: savedSuggestions }))
-
+        .then(()=>Actions.home());//instead of calling "display" we just redirect to home. 
 }
 
-export const getBooksFromApi = (books, userID, dispatch) => {
-    const bookPromises = books.map((book) =>
-        axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book.Name}&key=${GOOGLE_API_KEY}`));
-        return axios.all(bookPromises)
+export const getBooksFromApi = (books) => {//we get an array of titles and return an array of book data
+    const bookPromises = books.map((book) => axios.get(`https://www.googleapis.com/books/v1/volumes?q=${book.Name}&key=${GOOGLE_API_KEY}`));
+    //bookPromises is an array of unresolved promises
+    return axios.all(bookPromises)
             .then(axios.spread((...args) => {
-                //collect returned data for each api call in array
+                //axios.spread((...args) returns an array of resolved promises, meaning an array of book objects
+                //we map through the array and pick out only the data we need, we return an array of book data 
                 return args.map((book) => {
-                       //return book.data.items[0].volumeInfo;
-                       //returning only the data we need
                     return {
-                                title:book.data.items[0].volumeInfo.title,
-                                author: book.data.items[0].volumeInfo.authors[0],
-                                imageLinks:book.data.items[0].volumeInfo.imageLinks ? book.data.items[0].volumeInfo.imageLinks: null,
-                                description: book.data.items[0].volumeInfo.description ? book.data.items[0].volumeInfo.description: null
-                             }
+                        title:book.data.items[0].volumeInfo.title,
+                        author: book.data.items[0].volumeInfo.authors[0],
+                        imageLinks:book.data.items[0].volumeInfo.imageLinks ? book.data.items[0].volumeInfo.imageLinks: null,//we have ternary cuz some books dont have image/descrition
+                        description: book.data.items[0].volumeInfo.description ? book.data.items[0].volumeInfo.description: null//without ternary, code crashes. unresolved promise error
+                    }
                 })
-            
             }))
             .catch((error) => {
                 console.error(error);
             });
 }
 
+export const find = (preferences) => {//we get an object of preferences and return an array of titles
+    //console.log(Object.keys(preferences), 'alala', preferences)
+    //using map so that we can collect unresolved promises in an arrary 
+    let titlePromises = Object.keys(preferences).map(keyword => {
+                            console.log(keyword, 'keyword')
+                            if (preferences[keyword] !== '') {
+                                console.log(preferences[keyword], 'check check')
+                                return axios.get(`https://tastedive.com/api/similar?q=${preferences[keyword]}&k=${TASTE_DIVE_API_KEY}&limit=2&type=${keyword}`)
+                            }
+                        })
 
-export const getDefualt = dispatch =>
-    dispatch =>
-        getBooksFromApi(defaultBooks.list, dispatch);//send default books to google api
-
-export const clearBooks = (dispatch) =>
-    dispatch => dispatch({ type: CLEAR_BOOKS });//clears preferences books array
-
-// export const findSimilar = (keyword, type, userID, dispatch) => {
-//    /* Object.keys(preferences).forEach(keyword =>{
-//        if (keyword !== '' ){
-//            axios.get(`https://tastedive.com/api/similar?q=${keyword}&k=${TASTE_DIVE_API_KEY}&limit=2&type=${keyword}s`)
-//         .then(res => getBooksFromApi(res.data.Similar.Results, userID, dispatch))
-//         .catch((error) => {
-//             console.error(error);
-//         });
-
-//        }
-//    })
-//    */
-
-
-//     console.log('findSimilar', keyword, type)
-//     let mediaType = null;
-//     type === 'title' ? mediaType = 'book' : mediaType = 'author';
-//     axios.get(`https://tastedive.com/api/similar?q=${keyword}&k=${TASTE_DIVE_API_KEY}&limit=2&type=${mediaType}s`)
-//         .then(res => getBooksFromApi(res.data.Similar.Results, userID, dispatch))
-//         .catch((error) => {
-//             console.error(error);
-//         });
-// }
-
-
-export const find = (preferences, userID, dispatch) => {
-    console.log(Object.keys(preferences), 'alala', preferences)
-    //using map so that we can collect and group all promises in an arraryto be returned to origin (origin is the function that started the chain of function calls) 
-    //map returns an array
-    return Object.keys(preferences).map(keyword => {
-        console.log(keyword, 'keyword')
-        if (preferences[keyword] !== '') {
-            console.log(preferences[keyword], 'check check')
-            return axios.get(`https://tastedive.com/api/similar?q=${preferences[keyword]}&k=${TASTE_DIVE_API_KEY}&limit=2&type=${keyword}`)
-                .then(res => {
-                    console.log(res, ' res!!!')
-                    return  getBooksFromApi(res.data.Similar.Results, userID, dispatch)
+    let titlePromises2 = titlePromises.filter(x => x);//removing undefined if any. cuz if we only update a title or a an author we get undefined for the other.even if that other pref
+    //already exists in db. its a glitch :D, once we fix glitch we can get rid of filter. 
+    //we return a single array of all titles to be sent to googleAPI     
+    return axios.all(titlePromises2)
+            .then((similarTitles)=>{
+                var titles=[];
+                // axios.all(promises) returned to us an array of objects, within object, there is a nested array -> object.data.Similar.Results, 
+                //which contains book titles, we push individual titles to "titles"
+                similarTitles.forEach((book)=>{
+                    book.data.Similar.Results.forEach((book)=>
+                        titles.push(book))
                 })
-                .catch((error) => {
-                    console.error(error);
-                });
-                    }
-    })
-}
-
-export const validate = (preferences) => {
-    if (preferences) {
-        //turns object values into array, then filters for non-empty, returns 0 if (object exists but is empty ) or a value
-        return Object.values(preferences).filter(preference => preference !== "").length;
-    } else return null //return null if no preferences object exists
+                return titles
+            }).catch((error) => {
+                console.error(error);
+            });        
 }
 
 
-export const loadPrefBooks = (userID, dispatch) => {
-    console.log('load pred')
+export const loadPrefBooks = (userID, dispatch) => {// we dont need to call a display function
     firebase.database().ref(`users/${userID}/`).child('preferences').once('value')
-     .then(snapshot => {
+    .then(snapshot => {
         const preferences = snapshot.val();//this is the  preferences object
-        if (validate(preferences)) {
-            //find user's preferences
-            return find(preferences, userID, dispatch)
-
-        } else {
-            console.log("no prefs, loading defualt suggestions");
-            return getBooksFromApi(defaultBooks.list, userID, dispatch);
-        }
+        if (!snapshot.val())
+            throw ("Error")
+                    
+        return find(preferences);//calling TasteDive
+        //we return an array of titles to be sent to googleAPI
     })
+    .then ((titles)=>{
+        return  getBooksFromApi(titles)//calling googleAPI
+    })
+    .then((booksData)=>{
+        saveSuggestions(booksData, userID, dispatch)//saving
+    })  
+    .catch(error => {
+        console.log("no prefs, loading defualt suggestions"); 
+        //we have a defualt branch in firebase, 
+        firebase.database().ref(`default`).once('value', (snapshot) => {
+                const defaultBooks=snapshot.val();
+                firebase.database().ref(`users/${userID}/`).child('suggestions').set([...defaultBooks]);//setting defualt books to suggestions branch  
+            })
+        })
 }
 
 
 
 
 
-export const getSuggestions = (userID, dispatch) =>
+export const getSuggestions = (userID, dispatch) =>//we call this function in componentWillMount, so we dont need to call a display function
     dispatch =>
-        firebase.database().ref(`users/${userID}/suggestions`).once('value')
+        firebase.database().ref(`users/${userID}/suggestions`).once('value')//if there are suggestions we load those to state
             .then(function (snapshot) {
                 if (!snapshot.val())
                     throw ("Error")
+                
                 const suggestions = Object.values(snapshot.val());
                 dispatch({ type: UPDATE_SUGGESTIONS, payload: suggestions })
-               // displaySuggestions(userID, dispatch)
             })
-            .catch((error) => {
-                loadPrefBooks(userID, dispatch)
-
-            //.then( displaySuggestions(userID, dispatch))
-//                // displaySuggestions(userID, dispatch)
-//                Promise.all([loadPrefBooks]).then(values => {
-//   console.log(values, 'val, va'); // [3, 1337, "foo"]
-
-})
-
-// .then(() => displaySuggestions(userID, dispatch))
-// })
-            // })
-            //  .then(() => {
-
-
-            //      return displaySuggestions(userID, dispatch)})
-
-// load pred
-// preferencesActions.js:84 display Suggestions *** nnnnn
-// preferencesActions.js:195 no prefs, loading defualt suggestions
-// preferencesActions.js:103 34
-
-// ref.child('blogposts').child(id).once('value')
-
-// .then(function(snapshot) {
-//   // The Promise was "fulfilled" (it succeeded).
-//   renderBlog(snapshot.val());
-// }, function(error) {
-//   // The Promise was rejected.
-//   console.error(error);
-// });
+            .catch((error) => loadPrefBooks(userID, dispatch))//else check for preferences, if none, load default
