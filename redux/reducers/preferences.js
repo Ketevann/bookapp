55c18f455f4ead3629632e3abe68e4fbf18bf1c1
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2018-01-05 23:49:06 -0500

    removed obsolete reducers

--- redux/reducers/preferences.js
+++ redux/reducers/preferences.js
@@ -1,38 +1,19 @@
 const INITIAL_STATE = { preferenceType: null, keyWord:'', title: '', author: '', genre: '', loading: true , books:false,  preferences: null, hasSugguestions:false}
 import {
- UPDATE_TITLE,
- UPDATE_AUTHOR,
- UPDATE_GENRE,
- CLEAR_BOOKS,
  UPDATE_PREFERENCE_TYPE,
  UPDATE_PREFERENCE_KEYWORD,
- UPDATE_SUGGESTIONS,
- HAS_SUGGESTIONS
+ UPDATE_SUGGESTIONS
 } from '../actions/action-types'
 
 
 export default (preferences = INITIAL_STATE, action) => {
   switch (action.type) {
-    case UPDATE_TITLE:
-      //alert("UPDATE_TITLE");
-      return { ...preferences, title: action.payload };
-    case UPDATE_AUTHOR:
-      //alert("UPDATE_AUTHOR");
-      return { ...preferences, author: action.payload };
-    case UPDATE_GENRE:
-      //alert("UPDATE_GENRE");
-      return { ...preferences, genre: action.payload };
     case UPDATE_PREFERENCE_KEYWORD:
-      //alert("UPDATE_GENRE");
       return { ...preferences, keyWord: action.payload };
     case UPDATE_PREFERENCE_TYPE:
-      return { ...preferences, preferenceType: action.payload }
+      return { ...preferences, preferenceType: action.payload };
     case UPDATE_SUGGESTIONS:
-      //alert(action.payload)
-      return {...preferences, preferences:action.payload}
-      case HAS_SUGGESTIONS:
-        return {...preferences, preferences:action.payload}
-
+      return {...preferences, preferences:action.payload};
     default:
       return preferences
   }
